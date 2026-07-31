f73e5148671cbacfa582c9566d643d061a578b3c
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-07-31 10:12:13 -0300

    Update UES_WEB_APP_URL to the production link

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * Cole abaixo a URL da implantação do Google Apps Script terminada em /exec.
  * Exemplo: https://script.google.com/macros/s/AKfycb.../exec
  */
-window.UES_WEB_APP_URL = 'COLE_AQUI_O_LINK_DO_WEB_APP_TERMINADO_EM_EXEC';
+window.UES_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyQaGlkbgmum0PDgE6lKy-bDEMgrEUGKDDzQYwPzOJzzdSpc7g1uh4H0GpMmOL9QA768A/exec';
