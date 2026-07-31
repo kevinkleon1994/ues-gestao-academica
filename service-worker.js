38ce2b3ede47d0a0f314021847785d472359b1d5
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-07-31 12:35:52 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE='ues-pwa-v31-1-international-phone-fix';
-const ASSETS=['./','./index.html','./offline.html','./manifest.webmanifest','./config.js','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./favicon-16.png','./favicon-32.png','./favicon-48.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  const u=new URL(e.request.url);
-  if(u.origin===self.location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>caches.match('./offline.html'))));}
-});
