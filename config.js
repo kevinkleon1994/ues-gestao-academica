edcea242709590a5d876e1ea4f9914bfe0ce264f
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-07-31 14:08:36 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Cole abaixo a URL da implantação do Google Apps Script terminada em /exec.
- * Exemplo: https://script.google.com/macros/s/AKfycb.../exec
- */
-window.UES_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyQaGlkbgmum0PDgE6lKy-bDEMgrEUGKDDzQYwPzOJzzdSpc7g1uh4H0GpMmOL9QA768A/exec';
