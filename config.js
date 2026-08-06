48a4cd968d544d5debe99092fcd5ff9a0435431d
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-06 05:21:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Cole abaixo a URL da implantação do Google Apps Script terminada em /exec.
  * Exemplo: https://script.google.com/macros/s/AKfycb.../exec
  */
-window.UES_WEB_APP_URL = 'COLE_AQUI_O_LINK_DO_WEB_APP_TERMINADO_EM_EXEC';
+window.UES_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwvA75cd1B4rkY6Pi9poSnkzFVWcyvb9uu1xoGUCp5vrC_5kQ-Gs2wmL3BztjiToAnyhw/exec';
 
 window.UES_VERSION = "2.3.0";
 
