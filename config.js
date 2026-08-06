f37baf1c8f9a6ee56f4871e8e1c82c2daffd4962
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-06 01:44:48 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/**
+ * Cole abaixo a URL da implantação do Google Apps Script terminada em /exec.
+ * Exemplo: https://script.google.com/macros/s/AKfycb.../exec
+ */
+window.UES_WEB_APP_URL = 'COLE_AQUI_O_LINK_DO_WEB_APP_TERMINADO_EM_EXEC';
