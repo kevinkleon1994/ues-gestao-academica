09be46de44fc22491c34fe002ca673eaa3dd9820
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-06 05:19:13 -0300

    Add files via upload
    Versão v3.0.3

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Cole abaixo a URL da implantação do Google Apps Script terminada em /exec.
+ * Exemplo: https://script.google.com/macros/s/AKfycb.../exec
+ */
+window.UES_WEB_APP_URL = 'COLE_AQUI_O_LINK_DO_WEB_APP_TERMINADO_EM_EXEC';
+
+window.UES_VERSION = "2.3.0";
+
+window.UES_APP_VERSION = '3.0.0';
