af24530ac9bfc9a3f07541a5c74126bea6241a73
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-06 01:22:10 -0300

    Update cache name for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ues-pwa-v34-interface-tools';
+const CACHE='ues-pwa-v35-login-install-modal';
 const ASSETS=['./','./index.html','./offline.html','./manifest.webmanifest','./config.js','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./favicon-16.png','./favicon-32.png','./favicon-48.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
